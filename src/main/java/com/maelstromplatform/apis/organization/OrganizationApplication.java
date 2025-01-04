32d55693b891ce9b096f970c160b8dafcff5abed
Jason Alan Smith <jason.alan.smith@outlook.com>
2025-01-04 00:03:41 -0600

    lint: fix lint issues

--- src/main/java/com/maelstromplatform/apis/organization/OrganizationApplication.java
+++ src/main/java/com/maelstromplatform/apis/organization/OrganizationApplication.java
@@ -3,11 +3,19 @@ package com.maelstromplatform.apis.organization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Maelstrom Platform Organization Service API.
+ */
 @SpringBootApplication
 public class OrganizationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrganizationApplication.class, args);
-	}
-
+  /**
+   * The entry point to the Organization service API.
+   *
+   * @param args The arguments to the entry point.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(
+        OrganizationApplication.class, args);
+  }
 }
